ee14a2e087193ab58726ca646bad813f4fcd5eb4
Fanny Ullén <fanny.ullen@outlook.com>
2026-07-17 18:47:08 +0200

    Added navbar

---
+++ app/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { nav } from "framer-motion/client";
+
+export default function Navbar() {
+    return (
+        <nav className="bg-slate-800 text-white p-4">
+            <h1 className="text-2xl font-bold">
+                TaskFlow
+            </h1>
+        </nav>
+    );
+}
