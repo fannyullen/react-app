da116d3b730256c272dd8aa876c79ef4c7b9c66d
Fanny Ullén <fanny.ullen@outlook.com>
2026-07-17 19:36:10 +0200

    Updated sidebar

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,20 @@
 export default function Sidebar() {
+    const menuItems = [
+        "Dashboard",
+        "Projects",
+        "Tasks",
+        "Settings"
+    ];
+
     return (
         <aside className="w-64 bg-white shadow-md">
             <nav className="p-6">
                 <ul className="space-y-4 text-gray-300">
-                    <li className="cursor-pointer rounded-md p-2 hover-bg-slate-200">Dashboard</li>
-                    <li className="cursor-pointer rounded-md p-2 hover-bg-slate-200">Projects</li>
-                    <li className="cursor-pointer rounded-md p-2 hover-bg-slate-200">Tasks</li>
-                    <li className="cursor-pointer rounded-md p-2 hover-bg-slate-200">Settings</li>
+                    {menuItems.map((item) => (
+                        <li 
+                        key={item}
+                        className="cursor-pointer rounded-md p-2 hover:bg-slate-200">{item}</li>
+                    ))}
                 </ul>
             </nav>
         </aside>
--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <Navbar />
         <div className="min-h-screen flex bg-gray-100">
           <Sidebar />
-          <main>
+          <main className="flex-1 p-8 text-2xl text-gray-300">
           {children}
           </main>
         </div>
