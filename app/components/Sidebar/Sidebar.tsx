8fec4c97bd4aa5a526ca9d1e4994338a85e277bd
Fanny Ullén <fanny.ullen@outlook.com>
2026-07-17 19:52:49 +0200

    Added links

--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -1,19 +1,40 @@
+import Link from "next/link";
+
 export default function Sidebar() {
     const menuItems = [
-        "Dashboard",
-        "Projects",
-        "Tasks",
-        "Settings"
+        {
+            label: "Dashboard",
+            href: "/"
+        },
+        {
+            label: "Projects",
+            href: "/projects"
+        },
+        {
+            label: "Tasks",
+            href: "/tasks"
+        },
+        {
+            label: "Settings",
+            href: "/settings"
+        },
     ];
 
     return (
         <aside className="w-64 bg-white shadow-md">
             <nav className="p-6">
                 <ul className="space-y-4 text-gray-300">
+                    {/*För varje objekt i menuItems, skapa ett nytt li-element*/}
+                    {/* Key = för att hålla reda på varje element i listan */}
                     {menuItems.map((item) => (
-                        <li 
-                        key={item}
-                        className="cursor-pointer rounded-md p-2 hover:bg-slate-200">{item}</li>
+                        <li key={item.href}>
+                            <Link
+                            href={item.href}
+                            className="cursor-pointer rounded-md p-2 hover:bg-slate-200"
+                            >
+                                {item.label}
+                            </Link>
+                        </li>
                     ))}
                 </ul>
             </nav>
