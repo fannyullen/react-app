e54d5ed0b9000f1dc053a89b0f3b93669b677840
Fanny Ullén <fanny.ullen@outlook.com>
2026-07-20 16:07:30 +0200

    Hamburger menu

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,14 +2,18 @@
 
 import { nav } from "framer-motion/client";
 import { useTheme } from "@/app/src/hooks/useTheme";
+import { FiMenu } from "react-icons/fi";
 
 export default function Navbar() {
     const { theme, toggleTheme } = useTheme();
     return (
-        <nav className="flex items-center justify-end md:justify-between px-6 py-4 text-white">
+        <nav className="flex items-center justify-between px-6 py-4 text-white">
             <h1 className="hidden md:block text-2xl font-bold">
                 TaskFlow
             </h1>
+            <div className="md:hidden text-2xl">
+                <FiMenu />
+            </div>
             <div className="flex gap-4">
                 <button className="cursor-pointer" onClick={toggleTheme}>
                     {theme === "dark" ? "☀️" : "🌙"}
