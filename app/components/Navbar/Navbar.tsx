d1af1f4f59b1b1a81226eb2b58283f9e622e8c7f
Fanny Ullén <fanny.ullen@outlook.com>
2026-07-20 15:46:53 +0200

    Design change

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { useTheme } from "@/app/src/hooks/useTheme";
 export default function Navbar() {
     const { theme, toggleTheme } = useTheme();
     return (
-        <nav className="flex items-center justify-between px-6 py-4 text-white bg-white/10 backdrop-blur-xl border border-white/20 shadow-2xl">
+        <nav className="flex items-center justify-between px-6 py-4 text-white">
             <h1 className="text-2xl font-bold">
                 TaskFlow
             </h1>
--- app/components/Sidebar/Sidebar.tsx
+++ app/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ import { menuItems } from "@/app/data/menuItem";
 export default function Sidebar() {
 
     return (
-        <aside className="bg-white/10 w-64 m-4 backdrop-blur-xl border border-white/20 shadow-2xl rounded-2xl">
+        <aside className="hidden md:block bg-white/10 w-64 m-4 backdrop-blur-xl border border-white/20 shadow-2xl rounded-2xl">
             <nav className="p-6">
                 <ul className="space-y-4 text-gray-400 mb-6">
                     {/*För varje objekt i menuItems, skapa ett nytt li-element*/}
